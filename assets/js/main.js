7483e05b3a820bf1634a0106e85b151c1f381e88
Sajib Hossain <sajibhossain1185@gmail.com>
2022-04-08 09:30:38 +0600

    menu done

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+/* =============== scroll menu =============== */
+const header = document.querySelector('.header');
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= 50) {
+        header.classList.add('active__nav');
+    } else {
+        header.classList.remove('active__nav');
+    }
+})
+
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu')
+
+/*===== REMOVE MENU MOBILE =====*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
